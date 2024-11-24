896683d161de9a3fe78b6e2c37c4623ac39fee23
Delta Student <kumari021deepali@gmail.com>
2024-11-24 21:14:35 +0530

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import SignIn from './assets/pages/SignIn';
 import SignUp from './assets/pages/SignUp';
 import About from './assets/pages/About';
 import Profile from './assets/pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+  <Header />
   <Routes>
-    <Route path="/" element={<Home/>} />
-    <Route path="/sign-in" element={<SignIn/>} />
-    <Route path="/sign-up" element={<SignUp/>} />
-    <Route path="/about" element={<About/>} />
-    <Route path="/profile" element={<Profile/>} />
+    <Route path='/' element={<Home/>} />
+    <Route path='/sign-in' element={<SignIn/>} />
+    <Route path='/sign-up' element={<SignUp/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/profile' element={<Profile/>} />
   </Routes>
   </BrowserRouter>
+  );
 }
