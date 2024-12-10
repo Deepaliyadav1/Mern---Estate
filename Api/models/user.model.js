f6998b5a2988f94c1ca2825de19fa3463abb340b
Delta Student <kumari021deepali@gmail.com>
2024-12-10 19:22:26 +0530

    create and add Oauth component

--- Api/models/user.model.js
+++ Api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  avatar:{
+    type: String,
+    default: "https://cdn3.pixelcut.app/7/18/profile_photo_maker_hero_100866f715.jpg"
+  },
 }, { timestamps: true }); // Properly closing the schema object with a bracket
 
 const User = mongoose.model('User', userSchema);
--- client/src/assets/pages/SignUp.jsx
+++ client/src/assets/pages/SignUp.jsx
@@ -58,6 +58,7 @@
 
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -104,6 +105,7 @@ export default function SignUp() {
         <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" onChange={handleChange} />
         <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" onChange={handleChange} />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">Sign Up</button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
