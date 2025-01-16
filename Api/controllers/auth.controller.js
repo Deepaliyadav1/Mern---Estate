8f2a79a17b58527486709cab92a42058d4ab9337
Delta Student <kumari021deepali@gmail.com>
2025-01-16 09:10:43 +0530

    create user update api

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
+    const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
@@ -53,7 +53,7 @@ export const signin = async (req, res, next) => {
 };
 export const google = async (req, res, next) => {
   try {
-    const user = await User.findOne({ email: req.body.email })
+    const user = await User.findOne({ email: req.body.email });
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
@@ -74,4 +74,4 @@ export const google = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-}
+};
