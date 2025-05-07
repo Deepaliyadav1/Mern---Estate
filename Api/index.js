6eb177012095dc20e0998672a10fc81c1246ee7b
Delta Student <kumari021deepali@gmail.com>
2025-05-07 21:05:51 +0530

    add pre installation

--- Api/index.js
+++ Api/index.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 dotenv.config();
 
 console.log("MongoDB URI:", process.env.MONGO);
@@ -17,6 +19,8 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to MongoDB:", err);
   });
+
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +35,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
